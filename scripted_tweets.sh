918c04a21d71723a84661ad2017bc45066141147
Sean Neal <seanneal@outlook.com>
2017-09-04 17:07:30 +0000

    adding a small shell script to run this thing hourly.

---
+++ scripted_tweets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cd ~/tweetbot/
+echo beginning tweet bot >> scripted_tweets.log
+date >> scripted_tweets.log
+git pull >> scripted_tweets.log
+python3 tweet_bot.py >>scripted_tweets.log
+date >> scripted_tweets.log
