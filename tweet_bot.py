ae5b7f9c4d0109b9eb4d3aca820d98c7bc6e967d
Sean Neal <seanneal@outlook.com>
2017-09-04 15:54:02 -0700

    did some refactoring to enable some future stuff and better filtering.

--- tweet_bot.py
+++ tweet_bot.py
@@ -1,11 +1,14 @@
 '''
 First Version: Tweets contents from subreddits
 '''
-import sys
 import time
 import configparser
+from collections import namedtuple
 import praw
 import tweepy
+
+Post = namedtuple('Post', ['id', 'title', 'url',
+                           'reddit_url', 'domain', 'user', 'subreddit_name'])
 
 
 def strip_title(title):
@@ -18,30 +21,34 @@ def strip_title(title):
         return title[:93] + "..."
 
 
-def tweet_creator(subreddit_info):
+def get_posts(reddit_connection, subreddit_name):
     '''
     grabs posts from reddit and formats them for processing
     '''
-    post_dict = {}
-    post_ids = []
-    exclude_domain = 'self.' + subreddit_info.display_name
+    posts = []
     print('[bot] Getting posts from Reddit')
-    for submission in subreddit_info.hot(limit=20):
-        if not submission.domain == exclude_domain:
-            post_dict[strip_title(submission.title)] = submission.url
-            post_ids.append(submission.id)
-    return post_dict, post_ids
+    subreddit = reddit_connection.subreddit(subreddit_name)
+    for submission in subreddit.hot(limit=20):
+        post = Post(id=submission.id,
+                    title=submission.title,
+                    url=submission.url,
+                    reddit_url=submission.shortlink,
+                    domain=submission.domain,
+                    user=submission.author.name,
+                    subreddit_name=submission.subreddit.display_name
+                    )
+        posts.append(post)
+    return posts
 
 
-def setup_connection_reddit(subreddit):
+def setup_reddit_connection():
     '''
     Gets a connection to the subreddit.
     uses the config in praw.ini  needs to be in working directory
     see: http://praw.readthedocs.io/en/latest/getting_started/configuration/prawini.html
     '''
     print('[bot1] setting up connection with Reddit')
-    reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
-    return reddit.subreddit(subreddit)
+    return praw.Reddit('bot1', user_agent='bot1 user agent')
 
 
 def duplicate_check(post_id):
@@ -67,12 +74,27 @@ def main():
     '''
     connects the pieces to grab posts from reddit and throw them on twitter
     '''
-    subreddit = setup_connection_reddit('ethereum')
-    post_dict, post_ids = tweet_creator(subreddit)
-    tweeter(post_dict, post_ids)
+    reddit_connection = setup_reddit_connection()
+    posts = get_posts(reddit_connection, 'ethereum')
+    tweeter(posts)
 
 
-def tweeter(post_dict, post_ids):
+def filter_posts(posts):
+    '''
+    filters the posts
+    '''
+    filtered_domains = {'imgur.com'}
+
+    def should_be_filtered(post):
+        '''
+        checks all relevant conditions
+        '''
+        return post.domain not in filtered_domains
+
+    return (x for x in posts if not should_be_filtered(x))
+
+
+def tweeter(posts):
     '''
     puts the posts on twitter
     '''
@@ -82,16 +104,16 @@ def tweeter(post_dict, post_ids):
     auth.set_access_token(config['access_token'],
                           config['access_token_secret'])
     api = tweepy.API(auth)
-    for post, post_id in zip(post_dict, post_ids):
-        if not duplicate_check(post_id):
+    for post in filter_posts(posts):
+        if not duplicate_check(post.id):
             print('[bot] Posting this link on twitter')
-            print(post + ' ' + post_dict[post] + ' #bot')
+            print(post.title + ' ' + post.url + ' #bot')
             try:
                 api.update_status(
-                    post + ' ' + post_dict[post] + ' #bot')
+                    post.title + ' ' + post.url + ' #bot')
             except Exception as e:
                 print(e)
-            add_id_to_file(post_id)
+            add_id_to_file(post.id)
             time.sleep(30)
 
 
