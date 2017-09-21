0bad00fe9a3e15adcc1b8abbe8d99ca89913bbf7
Sean Neal <seanneal@outlook.com>
2017-09-21 06:41:26 -0700

    making sure we create a new log with every run so that it stops eating so much disk space.

--- scripted_tweets.sh
+++ scripted_tweets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ~/tweetbot/
-echo beginning tweet bot >> scripted_tweets.log
+echo beginning tweet bot > scripted_tweets.log
 date >> scripted_tweets.log
 git pull >> scripted_tweets.log
 python3 tweet_bot.py >>scripted_tweets.log
